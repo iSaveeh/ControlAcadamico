436270d3e0fbd98c0c6de4f73fcd1242e6bcea75
iSaveeh <SebastianIza10@outlook.com>
2025-07-20 18:27:43 -0500

    Navbar Menu

---
+++ screens/menu.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" /> 
+    <title>Plataforma Duver Freud FG</title>
+
+    <!-- Enlace al archivo CSS -->
+    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/menu.css">
+
+
+    <!-- Conexión previa a Google Fonts (mejora rendimiento) -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <!-- Fuente Inter desde Google Fonts, con soporte para cursiva e intermedios -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,400;1,400;1,700&display=swap" rel="stylesheet">
+
+    <style>
+        /* Aplica la fuente a todo el documento */
+        body {
+            font-family: 'Inter', sans-serif;
+        }
+    </style>
+</head>
+<body>
+
+    <!-- Barra de navegación principal -->
+   <div class="navbar">
+    
+    <!-- Bloque izquierda: Logo y título -->
+    <div class="logo-text">
+        <img src="../images/LogoColegio.png" class="logo" alt="Logo Colegio">
+        <div class="titulo">
+            <div class="superior">PsicoPedagógico</div>
+            <div class="inferior">Duver Freud</div>
+        </div>
+    </div>
+
+    <!-- Bloque derecha: Botón cerrar sesión -->
+    <div class="acciones-navbar" style="display: flex; align-items: center; gap: 20px;">
+
+        <!-- Línea separadora -->
+        <div class="separador"></div>
+
+        <!-- Boton de notificaciones -->
+        <button class="notificaciones">
+            <img src="../images/icons/notis.png" class="notificaciones-icon" alt="notificaciones logo">
+            <span class="noti-texto">Notificaciones </span>
+        </button>
+
+        <!-- Línea separadora -->
+        <div class="separador"></div>
+
+        <!-- Boton de perfil -->
+        <button class="perfil">
+            <img src="../images/icons/perfil.png" class="perfil-icon" alt="Perfil">
+            <span class="perfil-texto">Sebastian Iza</span>
+        </button>
+
+        <!-- Línea separadora -->
+        <div class="separador"></div>
+
+        <!-- Boton de Cerrar Sesion -->
+        <button class="cerrar-sesion">
+            <img src="../images/icons/cs.png" class="cs-icon" alt="Cerrar Sesión">
+            <span class="cerrar-texto">Cerrar Sesión</span>
+        </button>
+    </div>
+        </div>
+
+
+    <!-- Contenido principal -->
+     <div class="contenido">
+        <h1>Bienvenido Sebastian Iza</h1>
+        <p>Futuro contenido en la pagina</p>
+
+
+
+
+</body>
+</html>
